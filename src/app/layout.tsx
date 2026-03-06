a913cc02006e0a3ebb6d616ab0f23f54767c2fd7
Josuehd Snjsjiaia <midszreformes@gmail.com>
2026-03-06 17:24:55 +0000

    adicione o pixel do facebook + da utmify acima do head da pagina!
    preci

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,18 +20,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
         
         {/* Meta Pixel Code */}
-        <script dangerouslySetInnerHTML={{ __html: `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1256757253223112');
-          fbq('track', 'PageView');
-        ` }} />
+        <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '1256757253223112'); fbq('track', 'PageView');` }} />
         <noscript>
           <img 
             height="1" 
@@ -43,14 +32,7 @@ export default function RootLayout({
         </noscript>
         {/* End Meta Pixel Code */}
 
-        <script dangerouslySetInnerHTML={{ __html: `
-          window.pixelId = "6997348f230d36c4029e9793";
-          var a = document.createElement("script");
-          a.setAttribute("async", "");
-          a.setAttribute("defer", "");
-          a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
-          document.head.appendChild(a);
-        ` }} />
+        <script dangerouslySetInnerHTML={{ __html: `window.pixelId = "6997348f230d36c4029e9793"; var a = document.createElement("script"); a.setAttribute("async", ""); a.setAttribute("defer", ""); a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js"); document.head.appendChild(a);` }} />
       </head>
       <body className="font-body antialiased bg-background selection:bg-accent selection:text-primary">
         {children}
